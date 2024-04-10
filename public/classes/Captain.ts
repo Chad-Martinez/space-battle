9ea5c622ab44d2ecf0a83850527048d033e0e8f2
chadcito <145295470+chadcito@users.noreply.github.com>
2024-04-10 02:20:51 -0600

    refactored captain class to use new ship class

--- public/classes/Captain.ts
+++ public/classes/Captain.ts
@@ -1,46 +1,18 @@
 class Captain extends Component {
-  id: string = 'captain';
-  characters: Array<Character> = [
-    {
-      name: 'Leela',
-      class: 'leela',
-      image: 'leela.webp',
-    },
-    {
-      name: 'Zapp',
-      class: 'zapp',
-      image: 'zapp.webp',
-    },
-  ];
-  player = {
-    character: this.characters[0],
-    ship: {},
-  };
-  ship: Ship;
+  captain: Character;
+  ship: Vessel;
 
-  set playerCaptain(captain: string) {
-    if (captain == 'leela') {
-      this.player.character = this.characters[0];
-    } else {
-      this.player.character = this.characters[1];
-    }
-  }
-
-  // set playerShip(ship: string) {
-  //   if (ship == 'planet-express') {
-  //     this.player.ship
-  //   }
-  // }
-
-  constructor(renderHookId: string, ship: Ship) {
+  constructor(renderHookId: string) {
     super(renderHookId);
-    this.ship = ship;
+    this.captain = App.player.player.captain;
+    this.ship = App.player.player.ship;
+    this.render();
   }
 
   render() {
     this.createRootElement('div', 'col', [{ name: 'id', value: 'captain' }]);
-    new Dialog('captain', 'captain');
-    this.ship.render();
+    new Dialog('captain', 'captain', this.captain);
+    new Ship('captain', 'captain', this.ship);
     new Controls('captain', 'captain');
   }
 }
