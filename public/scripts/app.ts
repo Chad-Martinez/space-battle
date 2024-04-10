ba77d8f3722d306e43f33e91e0beb2a22d0f2949
chadcito <145295470+chadcito@users.noreply.github.com>
2024-04-10 02:16:48 -0600

    refactored app.ts to use static player class

--- public/scripts/app.ts
+++ public/scripts/app.ts
@@ -1,30 +1,10 @@
 class App extends Component {
-  static captainModal: Modal;
-  static player: Captain;
-  static ship: Ship;
+  static player: Player;
+
   static init() {
-    const vessel = new Ship(20, 4, 0.7);
-    const captain = new Captain('game-area', vessel);
-    const chooseCaptain = new Modal('app', captain);
-    this.player = captain;
-    this.ship = vessel;
-    this.captainModal = chooseCaptain;
-  }
-
-  static handleChooseShip(selection: string) {
-    if (selection.includes('zapp')) {
-      this.ship = new Ship(30, 6, 0.5);
-    }
-    document.querySelector('.ship.modal')?.remove();
-    new GameBoard('app', this.player);
-  }
-
-  static handleChooseCaptain(selection: string) {
-    if (selection.includes('zapp')) {
-      this.player.playerCaptain = 'zapp';
-    }
-    document.querySelector('.captain.modal')?.remove();
-    new Modal('app', this.ship);
+    const player = new Player();
+    this.player = player;
+    new Modal('app', 'captain', this.player.captain);
   }
 }
 
